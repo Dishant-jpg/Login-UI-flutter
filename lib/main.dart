3cbaed9ea8544cfb373b8985d46252825daa250a
Dishant Gupta <84134946+Dishant-jpg@users.noreply.github.com>
2022-12-28 20:59:55 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,23 @@ void main() {
     },
   ));
 }
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp
+    Debug(
+      title: 'Flutter Demo',
+    );
+  }
+}
